eb3fd062bed24c5c46886340b22006f6c8231f96
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-11-28 11:56:52 +0000

    [Add]useコントローラ、Profileimage保存表示のための記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :profile_image
+
+  def get_profile_image(width, height)
+   unless profile_image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+   end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  #ユーザーごとのプロフィール画像を保存、プロフィール画像を扱うための記述↑
 end
 
 #:database_authenticatable, :registerable,」のように、devise の後ろに :（コロン）で始まる部分が devise の機能名です。
