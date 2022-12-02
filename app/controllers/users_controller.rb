102074ac9efdb545b487ee72fbec835ae4fbfad7
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-12-02 23:51:35 +0000

    [Create]show page,[Fix]def show @books

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def index
-    @book = Book.new  
+    @book = Book.new
     @user = User.find(params[:id])
   end
-  
+
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
