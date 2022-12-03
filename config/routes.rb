f8b98a677cab31b8a349ba1c30eb57ab2e9d00e7
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-12-03 05:49:40 +0000

    [Add]config.action_mailer.default_url_options = { host: 'localhost:3000' },[Add]usersにindex

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get "/about" => "homes#about", as: "about"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
   resources :books
 end
