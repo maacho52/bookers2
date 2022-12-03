996da8bc66c8c8dc6ba251665bfc9c6216caf416
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-12-03 04:54:20 +0000

    [Create]action(edit,update,destroy,book_params)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,30 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+
+  end
+
+
 end
