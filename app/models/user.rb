f0d5c58c9da9c31117cc3236d82347d7ef64ab18
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-12-03 10:51:51 +0000

    [Fix]layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :profile_image
-  
   has_many :books
+
+  has_one_attached :profile_image
 
   def get_profile_image(width, height)
    unless profile_image.attached?
