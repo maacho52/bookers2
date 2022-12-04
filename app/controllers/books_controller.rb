039f73b38e1769c86d0b0ffb974fa0fad51662df
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-12-04 08:37:28 +0000

    [Add}@user=current_user(index,show),createアクション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,30 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def edit
     @book = Book.find(params[:id])
   end
+
+  def show
+    @user = current_user
+    @books = @user.books
+  end
+
+  def create
+    @book = Book.new
+    @book.user_id = current_user.id
+    if @book.save
+      flash[ :notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :show
+    end
+  end
+
 
   def update
     @book = Book.find(params[:id])
