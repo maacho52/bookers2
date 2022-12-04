01b10b3db900483a3397d6113c1e35559d25aa51
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-12-04 08:38:47 +0000

    [adjustment]レイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @book = Book.new
     @user = User.find(params[:id])
+    @book = Book.new
     @books = Book.all
   end
 
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
      if @user.update(user_params)
@@ -21,12 +21,12 @@ class UsersController < ApplicationController
         redirect_to user_path(@user.id)
      end
   end
-  
+
   private
 
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
 end
