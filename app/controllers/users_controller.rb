d82084abe2ec7d377e0c525b4f2127e09343d259
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-12-04 09:00:31 +0000

    [Create]createアクション.createアクション内にsign_upした場合のフラッシュメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,16 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:notice] = "Welcome! You have signed up successfully."
+      redirect_to user_path(@user.id)
+    end  
+  end
+  
 
   def update
     @user = User.find(params[:id])
