188a39b3121630e3c9122b86d4772aed1eeeffc6
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-12-04 22:11:44 +0000

    [Delete]フラッシュメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,20 +16,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
-    if @user.save
-      flash[:notice] = "Welcome! You have signed up successfully."
-      redirect_to user_path(@user.id)
-    end
   end
 
 
   def update
     @user = User.find(params[:id])
-     if @user.update(user_params)
-        flash[:notice] = "You have updated user successfully."
-        redirect_to user_path(@user.id)
-     end
   end
 
   private
