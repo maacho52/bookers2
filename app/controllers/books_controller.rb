53646e5d86e7e8ad567dfae2876f44e7b9da6836
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-12-11 01:34:33 +0000

    [Fix]showアクション@booksを消して@bookを定義

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,18 +14,18 @@ before_action :correct_user, only: [:edit]
 
   def show
     @user = current_user
-    @books = @user.books
+    @book = Book.find(params[:id])
   end
 
   def create
-    @book = Book.new
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       flash[ :notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
-      render :index
+      render :show
     end
   end
 
