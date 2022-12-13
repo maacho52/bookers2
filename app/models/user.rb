adf3d9a8ec6aa255732e1a5deb742a62e99e40c4
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-12-13 11:28:23 +0000

    [Add]validatesのuniquenessを

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  validates :name, presence: true, length: { maximum: 20, minimun: 2, allow_blank: true }
-  validates :introduction, presence: true, length: { maximum: 50 }
+  validates :name, uniqueness: true, length: { minimum: 2, maximum: 10, allow_blank: true }
+  validates :introduction, length: { maximum: 50 }
 
   has_one_attached :profile_image
 
