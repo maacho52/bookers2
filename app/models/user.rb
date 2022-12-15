f0ef74063f4949beebb4e3aade2c6548f055d717
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-12-15 10:35:30 +0000

    [Fix]nameカラムのmaximumを10から20に

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  validates :name, uniqueness: true, length: { minimum: 2, maximum: 10 }
+  validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
 
 
