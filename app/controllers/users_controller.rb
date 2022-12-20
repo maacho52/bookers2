1349d0ab0fdab53ba1473381787ea502ac48e58d
maacho52 <117494270+maacho52@users.noreply.github.com>
2022-12-20 12:47:39 +0000

    [Fix]他人のユーザ情報編集画面 遷移できず、自分のユーザ詳細画面にリダイレクトされるようにcorrect_userのredirect_toを(user_path(current_user.id))に

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ before_action :correct_user, only: [:edit, :update]
 
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(books_path) unless @user == current_user
+    redirect_to(user_path(current_user.id)) unless @user == current_user
   end
 
 end
